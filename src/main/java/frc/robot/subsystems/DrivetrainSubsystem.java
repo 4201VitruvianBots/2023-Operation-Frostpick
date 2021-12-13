60648168c67ac5909a1332823f32ba997d7cba7f
Dh-Van <dhvan17shah@gmail.com>
2021-12-13 17:57:57 +0000

    refactored Drivetrain Subsystem

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -77,9 +77,10 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     public DrivetrainSubsystem() {
         ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+        initializeMotors(tab);
+    }
 
-        m_odometry = new SwerveDriveOdometry(m_kinematics, getGyroscopeRotation());
-
+    public void initializeMotors(ShuffleboardTab tab){
         m_frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
             // Allows you to see the current state of the module on the dashboard.
             tab.getLayout("Front Left Module", BuiltInLayouts.kList)
