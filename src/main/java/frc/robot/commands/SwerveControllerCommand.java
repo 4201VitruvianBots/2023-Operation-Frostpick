d3c27b07d588a5dab08f6bd2193051a46e677ef5
Dhvan <34456470+Dh-Van@users.noreply.github.com>
2022-01-07 12:34:48 -0500

    Fixed import stuff

--- src/main/java/frc/robot/commands/SwerveControllerCommand.java
+++ src/main/java/frc/robot/commands/SwerveControllerCommand.java
@@ -13,7 +13,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.trajectory.Trajectory.State;
 import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
